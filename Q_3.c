39e45e2d6f4947e6d4abe65d01d8a5b0983b6221
VrajKoladiya <vrajkoladiya07work@gmail.com>
2024-06-25 16:10:51 +0530

    Add files via upload

---
+++ Q_3.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+/*
+
+Input:
+Enter the array's row & column size: 3
+Enter array's elements:
+a[0][0] = 2
+a[0][1] = 4
+a[0][2] = 1
+a[1][0] = 3
+a[1][1] = 5
+a[1][2] = 4
+a[2][0] = 8
+a[2][1] = 2
+a[2][2] = 6
+
+
+Output:
+The transpose matrix of an array:
+2 3 8
+4 5 2
+1 4 6
+
+*/
+main()
+{
+	int r, c;
+	
+	printf("Enter the array's row & column size = ");
+	scanf("%d",&r);
+	c=r;
+	
+	int i, j, array[r][c];
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("a[%d][%d] = ",i,j);
+			scanf("%d",&array[i][j]);
+		}
+	}
+	
+	printf("\n---------------------------\n");
+	
+	printf("The transpose matrix of an array = \n");
+	for(j=0;j<c;j++)
+	{
+		for(i=0;i<r;i++)
+		{
+			printf("%d ",array[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+}
